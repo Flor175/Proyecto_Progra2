1d64f82fa57103a9c82ef06246e41b9d0225fce5
Flor175 <43833950+Flor175@users.noreply.github.com>
2019-06-03 16:09:19 -0600

    database

--- Proyecto/settings.py
+++ Proyecto/settings.py
@@ -86,7 +86,7 @@ else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': 'Proyectodb',
+            'NAME': 'proyectodb',
             'USER': 'proyecto_admin',
             'PASSWORD': 'progra2',
             'HOST': 'localhost',
