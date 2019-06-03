7c272628b9e869593a48de10d713745d9830d4a1
Flor175 <43833950+Flor175@users.noreply.github.com>
2019-06-03 12:42:32 -0600

    root changes

--- Proyecto/settings.py
+++ Proyecto/settings.py
@@ -75,12 +75,25 @@ WSGI_APPLICATION = 'Proyecto.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'Proyecto',
+            'USER': 'proyecto_admin',
+            'PASSWORD': 'progra2',
+            'HOST': 'localhost',
+            'PORT': '',
+        }
+    }
+
 
 
 # Password validation
@@ -120,7 +133,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 LOGIN_URL = '/iniciar-sesion/'
 LOGOUT_URL = '/cerrar-sesion/'
 LOGIN_REDIRECT_URL = '/Inicio/'
