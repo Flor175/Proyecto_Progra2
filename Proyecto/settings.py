36734e154750c4ed61f165f90a89832cb5214771
Flor175 <43833950+Flor175@users.noreply.github.com>
2019-06-03 15:00:10 -0600

    root

--- Proyecto/settings.py
+++ Proyecto/settings.py
@@ -135,8 +135,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 #STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+#PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+#STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 LOGIN_URL = '/iniciar-sesion/'
 LOGOUT_URL = '/cerrar-sesion/'
 LOGIN_REDIRECT_URL = '/Inicio/'
@@ -152,12 +152,15 @@ PASSWORD_RESET_TIMEOUT = 600
 MEDIA_URL = '/Archivos_Subidos/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'Archivos_Subidos')
 
-STATICFILES_DIRS = [ 
+if DEBUG:
+   STATICFILES_DIRS = [ 
     os.path.join(BASE_DIR, "templates/static"),
     ("automatas", os.path.join(BASE_DIR, "Archivos/static")),
 ]
-try: 
-  from local_settings import * 
-except: 
-  pass
+else:
+   STATIC_ROOT = os.path.join(BASE_DIR,'static')
+
+
+
+
 
