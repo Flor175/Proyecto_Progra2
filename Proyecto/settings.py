10f9de16ea60be5e08eaa6eb609e2ba5ad18aa4e
Flor175 <43833950+Flor175@users.noreply.github.com>
2019-06-03 14:51:57 -0600

    root

--- Proyecto/settings.py
+++ Proyecto/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'Proyecto.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'Proyecto/templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
