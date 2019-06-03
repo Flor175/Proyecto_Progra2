8a8d8c3853f52608db8f3e2bb9e5542a329286c1
Flor175 <43833950+Flor175@users.noreply.github.com>
2019-06-03 12:51:08 -0600

    debug

--- Proyecto/settings.py
+++ Proyecto/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '!uzk#cl!o^fsdcgf7o_llu#w#o=&boo7nccwr8b!otehubfqrj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['159.65.233.185']
 
